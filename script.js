41749dc4a7c4e57906546dfc055bef5160172292
mmaismma <mmaismma@hotmail.com>
2020-10-24 18:27:02 +0530

    It is functional now

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
 let picData = [];
+let tempArray = [];
 
 let httpRequest = new XMLHttpRequest();
-httpRequest.open('GET', 'https://www.reddit.com/r/rabbits/best.json');
+httpRequest.open('GET', 'https://www.reddit.com/r/rabbits/best.json?limit=80');
 httpRequest.send();
 httpRequest.onreadystatechange = () => {
     if (httpRequest.readyState === 4) {
-        let tempArray = JSON.parse(httpRequest.responseText).data.children;
+        tempArray = JSON.parse(httpRequest.responseText).data.children;
         tempArray.forEach(post => {
-            if (!post.data.stickied || !post.data.is_video || !post.data.over_18) {
-                picData.push(post)
+            if (!post.data.stickied) {
+                if (!post.data.is_video) {
+                    if (!post.data.over_18) {
+                        if (!post.data.gallery_data) {
+                            picData.push(post)
+                        }
+                    }
+                }
             }
         });
+        nextPic.click()
         console.log('done')
     }
 }
@@ -18,18 +26,24 @@ httpRequest.onreadystatechange = () => {
 const prevPic = document.getElementById('prev-pic');
 const nextPic = document.getElementById('next-pic');
 const shotModer = document.getElementById('shot-moder');
+const pic = document.getElementById('pic')
 
-let picNum = 0;
+let picNum = -1;
 
 function changePic(diff) {
     picNum += diff;
-    
+    picNum === 0 ? prevPic.disabled = true :prevPic.disabled = false
+    picNum === picData.length ? nextPic.disabled = true : nextPic.disabled = false
+
+    pic.style.animation = '';
+    pic.src = "";
+    pic.src = picData[picNum].data.url;
 }
 
 function toggleShotMode() {
     document.fullscreenElement === null ? document.body.requestFullscreen() : document.exitFullscreen()
 }
 
-prevPic.onclick = changePic.bind(-1);
-nextPic.onclick = changePic.bind(1);
-shotModer.onclick = toggleShotMode.bind();
+prevPic.onclick = () => {changePic(-1)}
+nextPic.onclick = () => {changePic(1)}
+shotModer.onclick = () => {toggleShotMode()}
