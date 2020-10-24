ddf41e08922f601783922d1a928f4cc639e88db6
mmaismma <mmaismma@hotmail.com>
2020-10-24 14:40:42 +0530

    🚗Initialised the project

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
+let picData = [];
 
+let httpRequest = new XMLHttpRequest();
+httpRequest.open('GET', 'https://www.reddit.com/r/rabbits/best.json');
+httpRequest.send();
+httpRequest.onreadystatechange = () => {
+    if (httpRequest.readyState === 4) {
+        let tempArray = JSON.parse(httpRequest.responseText).data.children;
+        tempArray.forEach(post => {
+            if (!post.data.stickied || !post.data.is_video || !post.data.over_18) {
+                picData.push(post)
+            }
+        });
+        console.log('done')
+    }
+}
+
+const prevPic = document.getElementById('prev-pic');
+const nextPic = document.getElementById('next-pic');
+
+let picNum = 0;
+
+prevPic.onclick = changePic(-1);
+nextPic.onclick = changePic(1);
+
+function changePic(diff) {
+    picNum += diff;
+}
